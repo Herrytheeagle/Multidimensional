3735b075a487aea65df6bb70a3439d2dd72fb6dc
herrytheeagle <herrytheeagle@gmail.com>
2017-10-06 11:09:31 +0100

    multidimensional arrays

---
+++ main.cpp
@@ -0,0 +1,40 @@
+
+/*#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int single[3]={1,2,3};
+    int multi[2][3] = {{12,13,12},{15,16,17}};
+    cout<<"_________"<<endl;
+    cout << multi[0][0]<<"this is index[0][0]"<<endl;
+     cout << multi[0][1]<<"this is index[0][1]"<<endl;
+      cout << multi[0][2]<<"this is index[0][2]"<<endl;
+      cout <<endl;
+      cout<<"________"<<endl;
+       cout << multi[1][0]<<"this is index[1][0]"<<endl;
+     cout << multi[1][1]<<"this is index[1][1]"<<endl;
+      cout << multi[1][2]<<"this is index[1][2]"<<endl;
+    cout<<endl;
+/* for(int row =0; row<2; row++){
+
+        for(int column=0; column<3; column++){
+            cout<<multi[row][column];
+            if(multi[row][column]==14){
+                cout <<endl;
+            }
+
+
+           }
+        }*/
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+int myint;
+myint = 0*00000001;
+printf ("%d \n" ,myint);
+
+    return 0;
+}
